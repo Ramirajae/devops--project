87943750fe814828a7036d9165f21af09de0f431
Ramirajae <rajaerami788@gmail.com>
2026-03-08 22:20:01 +0000

    Fix import test

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -2,7 +2,7 @@ import pytest
 import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-from app import app as flask_app
+from app.app import app as flask_app
 
 @pytest.fixture
 def client():
